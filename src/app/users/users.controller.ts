ce72832068a69938ebd6cd8bf9d26781399af7e5
ian <lyjin2007@gmail.com>
2022-08-25 10:25:28 +0900

    feat: Add swagger test in users

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserDto, UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @ApiOperation({ summary: '사용자 정보 조회' })
+  @ApiResponse({ status: 200, description: 'get all users' })
+  getAllUsers(): UserDto[] {
+    return this.usersService.getAllUsers();
+  }
+
+  @Post()
+  @ApiResponse({ status: 201, description: 'create user' })
+  createUser(@Body() createDto: UserDto) {
+    return this.usersService.createUser(createDto);
+  }
+}
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserDto {
+  @ApiProperty({ description: 'id' })
+  id: number;
+
+  @ApiProperty({ description: '이름' })
+  name: string;
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: UserDto[] = [];
+
+  getAllUsers(): UserDto[] {
+    return this.users;
+  }
+
+  createUser(createDto: UserDto) {
+    this.users.push({ id: this.users.length + 1, ...createDto });
+  }
+}
