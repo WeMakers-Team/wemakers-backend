1d6739d6fdbd49df033f5efaac82b4f578817a2e
ian <lyjin2007@gmail.com>
2022-11-22 21:37:24 +0900

    refactor: refactoring User Service

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,20 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from '@prisma/client';
-import { FindUserResponse } from 'src/common/interface/users.interface';
+import { Account } from 'src/common/interface/auth.interface';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async findUser(userId: number): Promise<FindUserResponse> {
-    const user = await this.usersRepository.findUserByIdOrEmail(userId);
+  async findUser(userId: number): Promise<Account> {
+    const { password, ...response } =
+      await this.usersRepository.findUserByIdOrEmail(userId);
 
-    return {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    };
+    return response;
   }
 }
