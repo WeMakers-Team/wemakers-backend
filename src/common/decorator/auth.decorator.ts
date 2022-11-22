4304ad57e0197c99fee77ecfe36e710ee8147633
ian <lyjin2007@gmail.com>
2022-11-22 20:41:02 +0900

    feat: Make returns user id

--- src/common/decorator/auth.decorator.ts
+++ src/common/decorator/auth.decorator.ts
@@ -1,10 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetCurrentUser = createParamDecorator(
-  (data, ctx: ExecutionContext) => {
+  (data: string, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
-    const user = req?.user;
 
-    return data ? user?.[data] : user;
+    return req[data] || req.user; // id
   },
 );
