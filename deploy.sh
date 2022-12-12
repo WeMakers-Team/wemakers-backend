397cb612406bd3c9e8702aac717e1344d19247e7
CHOIJEWON <alsrn6040@kakao.com>
2022-12-12 10:20:10 +0900

    fix: ram 조정

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ sudo chown -R ubuntu:ubuntu wemakers
 cd /home/ubuntu/wemakers
 yarn install
 yarn generate:prisma
-yarn build NODE_OPTIONS=--max-old-space-size=8192
+yarn build
 yarn pm2 kill
 yarn start:pm2:dev
